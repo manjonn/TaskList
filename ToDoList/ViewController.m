c05a34b4e9fc91cbcbc3058042acf9679f978db1
manjonn <manjonn@gmail.com>
2015-07-19 21:12:55 -0700

    Add edit and delete rows

--- ToDoList/ViewController.m
+++ ToDoList/ViewController.m
@@ -8,9 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()<UITableViewDataSource>
+@interface ViewController ()<UITableViewDataSource,UITextFieldDelegate,UITableViewDelegate>
 
-@property(nonatomic,copy)NSArray *tasks;
+///NSArray is static array NSMutableArray Objects can be added and removed.
+@property(nonatomic,strong)NSMutableArray *tasks;
+@property (weak, nonatomic) IBOutlet UITableView *taskTableView;
+@property(nonatomic,assign)NSInteger editingIndex;
+@property (weak, nonatomic) IBOutlet UITextField *taskTextField;
 
 @end
 
@@ -18,7 +22,8 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.tasks=@[@"Finalize specs",@"Start techinical design",@"Get backend calls",@"Start coding"];
+    self.tasks=[NSMutableArray array];
+    self.editingIndex=-1;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -38,6 +43,75 @@
     UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"taskCell" forIndexPath:indexPath];
     cell.textLabel.text=self.tasks[indexPath.row];
     return cell;
+}
+
+#pragma mark - UITableViewDataSource
+/*
+ //Use for pre-iOS 8 versions
+
+ -(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
+    return YES;
+}
+ */
+
+/*
+ 
+ 
+ 
+*/
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    /*
+     //Use for pre-iOS 8 versions
+
+     if (UITableViewCellEditingStyleDelete) {
+        [self.tasks removeObjectAtIndex:indexPath.row];
+        [self.taskTableView reloadData];
+    }
+     */
+}
+
+-(NSArray *)tableView:(UITableView *)tableView editActionsForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    __weak typeof(self) weakself=self;
+    
+    UITableViewRowAction *deleteAction=[UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive title:@"Delete" handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+        
+        [weakself.tasks removeObjectAtIndex:indexPath.row];
+        [weakself.taskTableView reloadData];
+        
+    }];
+    UITableViewRowAction *editAction=[UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"Edit" handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+        
+        weakself.editingIndex=indexPath.row;
+        weakself.taskTextField.text=weakself.tasks[indexPath.row];
+        [weakself.taskTableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationNone];
+
+        
+    }];
+    
+    return @[deleteAction,editAction];
+    
+}
+
+
+
+
+#pragma mark - UITextFieldDelegate
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if (textField.text.length==0) {
+        return NO;
+    }
+    if (self.editingIndex<0) {
+        [self.tasks addObject:textField.text];
+    }else{
+        [self.tasks replaceObjectAtIndex:self.editingIndex withObject:textField.text];
+    }
+    [self.taskTableView reloadData];
+    textField.text=@"";
+    [textField resignFirstResponder];
+    return YES;
 }
 
 @end
